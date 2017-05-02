35fa9b219877d32ecdf389cacc684a94bf85225e
Quantum_Mechanics <Kraken_Labs@outlook.com>
2017-05-02 18:39:42 +0200

    Add signature verification example

---
+++ examples/nodejs/verifySignature.js
@@ -0,0 +1,20 @@
+// Include the library
+var nem = require("../../build/index.js").default;
+
+// Create keypair
+var kp = nem.crypto.keyPair.create("790c566699d4add2462f78bb108ca99b7e964098914a336710696db7f04d67a4");
+
+// Data to sign
+var data = "NEM is awesome !"
+
+// Sign data
+var sig = kp.sign(data);
+
+// Review
+console.log("Public key: " + kp.publicKey.toString());
+console.log("Original data: " + data);
+console.log("Signature: " + sig.toString());
+
+// Result
+console.log("\nIs signature valid ? ");
+console.log(nem.crypto.keyPair.verify(kp.publicKey.toString(), data, sig.toString()));
