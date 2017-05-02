56ee34d0b139b18f179ad544f68ae282eb3df441
Quantum_Mechanics <Kraken_Labs@outlook.com>
2017-05-02 18:31:55 +0200

    Fix circular dependency injection, added and improved prepareMessage

--- src/model/transactions.js
+++ src/model/transactions.js
@@ -5,6 +5,7 @@ import TransactionTypes from './transactionTypes';
 import Fees from './fees';
 import Serialization from '../utils/serialization';
 import KeyPair from '../crypto/keyPair';
+import CryptoHelpers from '../crypto/cryptoHelpers';
 import Requests from '../com/requests';
 
 /**
@@ -37,7 +38,7 @@ let NETWORK_VERSION = function(val, network) {
 let _multisigWrapper = function(senderPublicKey, innerEntity, due, network) {
 	let timeStamp = Helpers.createNEMTimeStamp();
     let version = NETWORK_VERSION(1, network);
-    let data = createCommonPart(TransactionTypes.multisigTransaction, senderPublicKey, timeStamp, due, version, network);
+    let data = _createCommonPart(TransactionTypes.multisigTransaction, senderPublicKey, timeStamp, due, version, network);
     let custom = {
         'fee': Fees.MultisigTransaction,
         'otherTrans': innerEntity
@@ -71,7 +72,7 @@ let _createCommonPart = function(txtype, senderPublicKey, timeStamp, due, versio
 /**
  * Prepare a transfer transaction object
  *
- * @param {object} common - A password/privateKey object
+ * @param {object} common - A common object
  * @param {object} tx - The un-prepared transfer transaction object
  * @param {number} network - A network id
  *
@@ -83,7 +84,7 @@ let prepareTransferTransaction = function(common, tx, network){
     let actualSender = tx.isMultisig ? tx.multisigAccount.publicKey : kp.publicKey.toString();
     let recipientCompressedKey = tx.recipient.toString();
     let amount = Math.round(tx.amount * 1000000);
-    let message = Helpers.prepareMessage(common, tx);
+    let message = prepareMessage(common, tx);
     let due = network === Network.data.testnet.id ? 60 : 24 * 60;
     let mosaics = null;
     let mosaicsFee = null
@@ -98,7 +99,7 @@ let prepareTransferTransaction = function(common, tx, network){
 /**
  * Prepare a mosaic transfer transaction object
  *
- * @param {object} common - A password/privateKey object
+ * @param {object} common - A common object
  * @param {object} tx - The un-prepared transfer transaction object
  * @param {object} mosaicDefinitionMetaDataPair - The mosaicDefinitionMetaDataPair object with properties of mosaics to send
  * @param {number} network - A network id
@@ -111,7 +112,7 @@ let prepareMosaicTransferTransaction = function(common, tx, mosaicDefinitionMeta
     let actualSender = tx.isMultisig ? tx.multisigAccount.publicKey : kp.publicKey.toString();
     let recipientCompressedKey = tx.recipient.toString();
     let amount = Math.round(tx.amount * 1000000);
-    let message = Helpers.prepareMessage(common, tx);
+    let message = prepareMessage(common, tx);
     let due = network === Network.data.testnet.id ? 60 : 24 * 60;
     let mosaics = tx.mosaics;
     let mosaicsFee = Fees.calculateMosaics(amount, mosaicDefinitionMetaDataPair, mosaics);
@@ -132,6 +133,7 @@ let prepareMosaicTransferTransaction = function(common, tx, mosaicDefinitionMeta
  * @param {number} due - The deadline in minutes
  * @param {array} mosaics - The array of mosaics to send
  * @param {number} mosaicFee - The fees for mosaics included in the transaction
+ * @param {number} network - A network id
  *
  * @return {object} - A [TransferTransaction]{@link http://bob.nem.ninja/docs/#transferTransaction} object
  */
@@ -173,8 +175,26 @@ let prepareImportanceTransferTransaction = function() {
 
 }
 
-let prepareApostilleTransaction = function() {
-
+/**
+ * Build a message object
+ *
+ * @param {object} common - An common object
+ * @param {object} tx - An un-prepared transferTransaction object
+ *
+ * @return {object} - A prepared message object
+ */
+let prepareMessage = function(common, tx) {
+    if (tx.encryptMessage && common.privateKey) {
+        return {
+            'type': 2,
+            'payload': CryptoHelpers.encode(common.privateKey, tx.recipientPubKey, tx.message.toString())
+        };
+    } else {
+        return {
+            'type': 1,
+            'payload': Helpers.isHexadecimal(tx.message.toString()) && tx.message.toString().substring(0, 2) === 'fe' ? tx.message.toString() : Convert.utf8ToHex(tx.message.toString())
+        };
+    }
 }
 
 /**
@@ -222,5 +242,6 @@ let prepare = function(objectName) {
 
 module.exports = {
     prepare,
-    send
+    send,
+    prepareMessage
 }
