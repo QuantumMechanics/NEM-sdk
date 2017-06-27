77a1178c14e70e81f1bcff67a70d7bbe9478b3fa
Quantum_Mechanics <Kraken_Labs@outlook.com>
2017-06-27 21:49:59 +0200

    Added getVersion function

--- src/model/network.js
+++ src/model/network.js
@@ -72,9 +72,27 @@ let char2Id = function(startChar) {
     }
 }
 
+/**
+ * Gets the network version
+ *
+ * @param {number} val - A version number (1 or 2)
+ * @param {number} network - A network id
+ *
+ * @return {number} - A network version
+ */
+let getVersion = function(val, network) {
+    if (network === data.mainnet.id) {
+        return 0x68000000 | val;
+    } else if (network === data.testnet.id) {
+        return 0x98000000 | val;
+    }
+    return 0x60000000 | val;
+}
+
 module.exports = {
     data,
     id2Prefix,
     id2Char,
-    char2Id
+    char2Id,
+    getVersion
 }
