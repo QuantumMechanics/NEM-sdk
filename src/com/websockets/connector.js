5f2a483719c974e1f1c00ddf7aeef9d1f417ef28
Quantum_Mechanics <Kraken_Labs@outlook.com>
2017-09-27 19:33:21 +0200

    Removed timeout fix in connector

--- src/com/websockets/connector.js
+++ src/com/websockets/connector.js
@@ -16,8 +16,6 @@ let create = function(endpoint, address) {
 		address: address.replace(/-/g, "").toUpperCase(),
 		socket: undefined,
         stompClient: undefined,
-        timeoutReconnect: undefined,
-        connectionAttempts: 0,
         connect: connect,
         close: close
 	}
@@ -25,47 +23,21 @@ let create = function(endpoint, address) {
 
 /**
  * Tries to establish a connection. 
- * After 10 failed attempts the promise will reject
  *
  * @return {promise} - A resolved or rejected promise
  */
 let connect = function() {
 	return new Promise((resolve, reject) => {
 		var self = this;
-		if(!SockJSBrowser) {
-			self.socket = new SockJSNode(self.endpoint.host + ':' + self.endpoint.port + '/w/messages');
-		} else {
-			self.socket = new SockJSBrowser(self.endpoint.host + ':' + self.endpoint.port + '/w/messages');
-		}
+		if (!SockJSBrowser) self.socket = new SockJSNode(self.endpoint.host + ':' + self.endpoint.port + '/w/messages');
+		else self.socket = new SockJSBrowser(self.endpoint.host + ':' + self.endpoint.port + '/w/messages');
 	    self.stompClient = Stomp.over(self.socket);
 	    self.stompClient.debug = false;
-	    // Timeout fix in case NIS socket is not responding
-	    let timeoutFix = setTimeout(() => {
-	    	reject("Not responding after 10 seconds!");
-	    }, 10000);
 	    self.stompClient.connect({}, function(frame) {
-	    	// Clear the timeout fix
-	    	clearTimeout(timeoutFix);
 		    resolve(true);
 	    }, (err) => {
-	    	// Clear the timeout fix
-	    	clearTimeout(timeoutFix);
-	    	// Add one attempt
-	    	self.connectionAttempts++;
-	        // Try to reconnect
-	        self.timeoutReconnect = setTimeout(() => {
-	        	if(self.connectionAttempts > 9) {
-	        		// Reset connection attempts
-	        		self.connectionAttempts = 0;
-	        		// Reject
-	        		reject("10 connection attempts failed!");
-	        	} else {
-			        console.log("Trying to reconnect...");
-			        // Tries to connect again
-			        resolve(self.connect());
-		    	}
-		    }, 1000);
-		});
+		    reject("Connection failed!");
+		});        	
 	});
 }
 
@@ -75,8 +47,6 @@ let connect = function() {
 let close = function() {
     var self = this;
     console.log("Connection to "+ self.endpoint.host +" must be closed now.");
-    // Stop trying to reconnect
-    clearTimeout(self.timeoutReconnect);
     self.socket.close();
     self.socket.onclose = function(e) {
         console.log(e);
